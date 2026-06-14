ba45c8d41ae14d37821ed0ad7d09d0d2cc6f9c9d
mariwpm <minkova.marija@gmail.com>
2026-06-14 20:02:14 +0300

    Haven version 3

--- sw.js
+++ sw.js
@@ -1,44 +1,28 @@
-const CACHE_NAME = 'safe-helper-v2';
-const ASSETS = [
+const CACHE = 'haven-v3';
+const FILES = [
   './',
   './index.html',
   './style.css',
   './fake.css',
   './app.js',
   './fake.js',
-  './manifest.webmanifest'
+  './manifest.webmanifest',
+  './favicon.svg'
 ];
 
-self.addEventListener('install', (event) => {
-  event.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
-  );
+self.addEventListener('install', (e) => {
+  e.waitUntil(caches.open(CACHE).then((c) => c.addAll(FILES)));
   self.skipWaiting();
 });
 
-self.addEventListener('activate', (event) => {
-  event.waitUntil(
-    caches.keys().then((keys) =>
-      Promise.all(keys.filter((k) => k !== CACHE_NAME).map((k) => caches.delete(k)))
-    )
-  );
+self.addEventListener('activate', (e) => {
+  e.waitUntil(caches.keys().then((ks) => Promise.all(ks.filter((k) => k !== CACHE).map((k) => caches.delete(k)))));
   self.clients.claim();
 });
 
-self.addEventListener('fetch', (event) => {
-  if (event.request.method !== 'GET') return;
-
-  event.respondWith(
-    caches.match(event.request).then((cached) => {
-      if (cached) return cached;
-      return fetch(event.request).then((response) => {
-        if (!response || response.status !== 200 || response.type !== 'basic') {
-          return response;
-        }
-        const clone = response.clone();
-        caches.open(CACHE_NAME).then((cache) => cache.put(event.request, clone));
-        return response;
-      }).catch(() => caches.match('./index.html'));
-    })
+self.addEventListener('fetch', (e) => {
+  if (e.request.method !== 'GET') return;
+  e.respondWith(
+    caches.match(e.request).then((hit) => hit || fetch(e.request).catch(() => caches.match('./index.html')))
   );
 });
